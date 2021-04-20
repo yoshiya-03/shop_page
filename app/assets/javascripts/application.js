a268ffe2bcee149cc18a5fa0576203a295248794
EC2 Default User <ec2-user@ip-172-31-40-76.ap-northeast-1.compute.internal>
2021-04-20 02:26:20 +0000

    [first commit]

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,46 @@
+/* global $*/
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+//= require jquery
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+
+document.addEventListener("turbolinks:load"
+, function () {
+$(function() {
+  $('.menu-trigger').on('click', function(event) {
+    $(this).toggleClass('active');
+    $('#sp-menu').fadeToggle();
+    event.preventDefault();
+  });
+});
+});
+
+
+ document.addEventListener("turbolinks:load"
+, function () {
+$(function() {
+  $('.a').slick({
+      fade: true,
+      autoplay: true, //自動再生
+      autoplaySpeed: 4000, //再生スピード
+      // 無限スクロールにするかどうか。最後の画像の次は最初の画像が表示される。
+      infinite: true,
+  });
+});
+});
