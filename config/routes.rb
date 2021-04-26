efd5f56d49ce5098b6886ee4ff0f80e2feb76fee
yoshiya-03 <yoshiya03y07@gmail.com>
2021-04-26 07:54:57 +0000

    [add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
  root 'pages#index'
  resources :pages
- 
+
  get 'look_images/look0'
- get 'lookimages/look1'
- get 'lookimages/look2'
- get 'lookimages/look3'
- get 'lookimages/look4'
- get 'lookimages/look5'
- get 'lookimages/look6'
- get 'lookimages/look7'
- get 'lookimages/look8'
- get 'lookimages/look9'
- get 'lookimages/look10'
- get 'lookimages/look11'
- get 'lookimages/look12'
-  
+ get 'look_images/look1'
+ get 'look_images/look2'
+ get 'look_images/look3'
+ get 'look_images/look4'
+ get 'look_images/look5'
+ get 'look_images/look6'
+ get 'look_images/look7'
+ get 'look_images/look8'
+ get 'look_images/look9'
+ get 'look_images/look10'
+ get 'look_images/look11'
+ get 'look_images/look12'
+
 end
